9a2fa7e21e6d73b9a06131d0bdbce6feb4331b38
iharsh220 <hgohil220@gmail.com>
2026-05-19 12:44:39 +0530

    all model name hasbeen changes due to not able to linked in code

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/database');
-const Admin = require('./Admin');
+const Admin = require('./admin');
 const Organogram = require('./organogram');
-const Event = require('./Event');
-const EventAssignment = require('./EventAssignment');
+const Event = require('./event');
+const EventAssignment = require('./eventAssignment');
 
 // Associations
 Organogram.hasMany(EventAssignment, { foreignKey: 'field_user_id', as: 'assignments' });
